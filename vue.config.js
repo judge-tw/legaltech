ad345e37a661169a42c99d898169123ea9c2b46f
Richo Han <Richo_Han@asus.com>
2019-08-28 22:19:55 +0800

    Enable GitHub Pages

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 const webpack = require('webpack');
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/legaltech_hackathon/'
+    : '/',
   configureWebpack: {
     // Set up all the aliases we use in our app.
     plugins: [
