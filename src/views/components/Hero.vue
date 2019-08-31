03db372e82d87df2f33e0864cca5d9cd452a9223
Richo Han <Richo_Han@asus.com>
2019-08-31 22:28:11 +0800

    Add transition animation

--- src/views/components/Hero.vue
+++ src/views/components/Hero.vue
@@ -25,7 +25,14 @@
                                         addon-left-icon="fa fa-search">
                             </base-input>
                             <div class="text-center">
-                                <base-button outline type="neutral" class="mb-3 mb-sm-0" icon="ni ni-atom">開始分析</base-button>
+                                <base-button
+                                    v-b-modal.modal-visualization
+                                    outline
+                                    type="neutral"
+                                    class="mb-3 mb-sm-0"
+                                    icon="ni ni-atom">
+                                    開始分析
+                                </base-button>
                             </div>
                         </form>
                         <!-- <div class="btn-wrapper">
@@ -62,10 +69,106 @@
                 </div>
             </div>
         </div>
+
+        <b-modal
+            centered
+            hide-backdrop
+            size="lg"
+            id="modal-visualization"
+            title="分析">
+            <!-- <b-card
+                title="Card Title"
+                img-src="https://picsum.photos/600/300/?image=25"
+                img-alt="Image"
+                img-top
+                tag="article"
+                style="max-width: 20rem;"
+                class="mb-2"
+            >
+                <b-card-text>
+                Some quick example text to build on the card title and make up the bulk of the card's content.
+                </b-card-text>
+
+                <b-button href="#" variant="primary">Go somewhere</b-button>
+            </b-card> -->
+            <template slot="default" slot-scope="{ }">
+                <b-container fluid class="text-light text-center">
+                    <b-row align-h="center">
+                        <b-col>
+                            <transition name="loading">
+                                <div v-if="loading">
+                                    <b>資料處理中</b>
+                                    <div>
+                                        <b-spinner style="width: 3rem; height: 3rem;" type="grow" label="Loading..."></b-spinner>
+                                    </div>
+                                </div>
+                            </transition>
+                            <transition name="loading">
+                                <div v-if="loaded">
+                                    <b>資料處理完成</b>
+                                </div>
+                            </transition>
+                        </b-col>
+                    </b-row>
+                </b-container>
+            </template>
+            <template slot="modal-footer" slot-scope="{ ok, cancel }">
+                <b-button size="md" variant="danger" @click="cancel()">
+                    離開
+                </b-button>
+                <b-button size="md" variant="success" @click="ok()">
+                    重新搜尋
+                </b-button>
+                <b-button size="md" variant="white" @click="prompt()">
+                    Test
+                </b-button>
+            </template>
+        </b-modal>
     </section>
 </template>
 <script>
-export default {};
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import loading from "./Loading.vue";
+
+Vue.use(BootstrapVue)
+
+export default {
+    data: function() {
+        return {
+            loading: true,
+            loaded: false
+        }
+    },
+    methods: {
+        prompt() {
+            this.loading = false;
+            var self = this;
+            setTimeout(function(){
+                self.loaded = true;
+            }, 1000);
+        }
+    },
+    mounted() {
+        this.$root.$on('bv::modal::show', (bvEvent, modalId) => {
+            console.log('Modal is about to be shown')
+        })
+        this.$root.$on('bv::modal::shown', (bvEvent, modalId) => {
+            console.log('Modal is shown')
+            this.prompt()
+        })
+    }
+};
 </script>
 <style>
+.loading-leave { opacity: 1; }
+.loading-leave-active { transition: opacity .5s }
+.loading-leave-to { opacity: 0; }
+.loading-enter { opacity: 0; }
+.loading-enter-active  { transition: opacity .5s }
+.loading-enter-to { opacity: 1; }
 </style>
---
+++ src/views/components/Loading.vue
@@ -0,0 +1,8 @@
+<template>
+    <b>資料處理中</b>
+</template>
+<script>
+
+export default {
+};
+</script>
