c1e776e00c0bdc26022642dfe405abe7caf7b3d9
Richo Han <Richo_Han@asus.com>
2019-09-07 11:03:57 +0800

    Deployment

---
+++ precache-manifest.2ec5373afb31bdc4a3b55a1b963f4c2b.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = [
+  {
+    "revision": "c85c75275c0a0a617f9e5accc2700908",
+    "url": "/legaltech/img/brand/creativetim-white-slim.png"
+  },
+  {
+    "revision": "be997d5226b992ffad34816870c6b7aa",
+    "url": "/legaltech/img/theme/team-2-800x800.jpg"
+  },
+  {
+    "revision": "4db1e474c948f13d9396",
+    "url": "/legaltech/js/chunk-505680fb.32e1003a.js"
+  },
+  {
+    "revision": "54e3f3c414bd8e7234bae3ee3be950e5",
+    "url": "/legaltech/img/theme/team-3-800x800.jpg"
+  },
+  {
+    "revision": "7f596f3a329a074d09bb",
+    "url": "/legaltech/js/chunk-ebdaf33a.0df7c471.js"
+  },
+  {
+    "revision": "66618a418175ddf2ac8c47a241d327a8",
+    "url": "/legaltech/img/theme/team-4-800x800.jpg"
+  },
+  {
+    "revision": "1d9c2615e5bb0e54d748",
+    "url": "/legaltech/js/chunk-vendors.a790940b.js"
+  },
+  {
+    "revision": "140e47cb804dfbce295f",
+    "url": "/legaltech/js/app.01972d58.js"
+  },
+  {
+    "revision": "7046659cad01e5cb7e51417e5fa7657b",
+    "url": "/legaltech/index.html"
+  },
+  {
+    "revision": "d7449878f28caa5dca19c3c7a1c6417a",
+    "url": "/legaltech/img/brand/lawsnote.svg"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/legaltech/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/legaltech/img/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "fd4a34d026fb9e0f4867188d47b11ba8",
+    "url": "/legaltech/img/theme/img-1-1200x1000.jpg"
+  },
+  {
+    "revision": "7789b5bfa57722dd8916b1b9ff1b1d37",
+    "url": "/legaltech/img/theme/img-2-1200x1000.jpg"
+  },
+  {
+    "revision": "edc7106b21ec12e57022b2ebd534cd2d",
+    "url": "/legaltech/img/theme/team-1-800x800.jpg"
+  },
+  {
+    "revision": "594b1ee1d95ada356eaad078e9217932",
+    "url": "/legaltech/img/ill/ill-2.svg"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/legaltech/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "dc49ad52655e1d9d0552c026db3ef688",
+    "url": "/legaltech/img/theme/landing.jpg"
+  },
+  {
+    "revision": "20d702b83a06bdb2ea71c4c0cb9a7a56",
+    "url": "/legaltech/img/theme/profile.jpg"
+  },
+  {
+    "revision": "974088a1931e40895bac6db119c62448",
+    "url": "/legaltech/img/theme/promo-1.png"
+  },
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "/legaltech/img/brand/favicon.png"
+  },
+  {
+    "revision": "185288d13ed8e9d745bd279ea34667bf",
+    "url": "/legaltech/img/brand/blue.png"
+  },
+  {
+    "revision": "8e55eab46b5fcfc4a7a0b27cb07c8888",
+    "url": "/legaltech/img/brand/github-white-slim.png"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/legaltech/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "6fafe4baca9d50d61a898c84ade7afa3",
+    "url": "/legaltech/img/brand/white.png"
+  },
+  {
+    "revision": "9289728f297355f179110ab27e124360",
+    "url": "/legaltech/img/brand/judge.png"
+  },
+  {
+    "revision": "a5a39f1f54a07d7b22df13eaeee770b1",
+    "url": "/legaltech/img/brand/moj.png"
+  },
+  {
+    "revision": "be9fe946ffa59bcf201c0d5bf4fbaaaf",
+    "url": "/legaltech/img/brand/law.png"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/legaltech/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/legaltech/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/legaltech/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/legaltech/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/legaltech/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/legaltech/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "/legaltech/favicon.png"
+  },
+  {
+    "revision": "1d9c2615e5bb0e54d748",
+    "url": "/legaltech/css/chunk-vendors.6c809cdf.css"
+  },
+  {
+    "revision": "7f596f3a329a074d09bb",
+    "url": "/legaltech/css/chunk-ebdaf33a.47b8dc24.css"
+  },
+  {
+    "revision": "140e47cb804dfbce295f",
+    "url": "/legaltech/css/app.e43c5e13.css"
+  }
+];
